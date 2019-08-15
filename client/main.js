4b02b53a790d0d790b6a4cf6357b30b270dfe76b
Vitche Research Team <developer@vitche.com>
2019-08-15 15:43:12 +0000

    1. Added peer list and edit modules.

--- client/main.js
+++ client/main.js
@@ -2,3 +2,5 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../modules/peer-edit/client/main.html'
+import '../modules/peer-list/client/main.html'
