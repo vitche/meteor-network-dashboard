54df98afd5b8f819416aa4d1ef60c60f22f75262
Vitche Research Team <developer@vitche.com>
2019-08-15 16:21:00 +0000

    1. Referenced the peer edit widget from the peer list widget.

--- client/main.js
+++ client/main.js
@@ -2,5 +2,5 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import '../modules/peer-edit/client/main.html'
-import '../modules/peer-list/client/main.html'
+import '../modules/peer-edit/client/widget.html'
+import '../modules/peer-list/client/widget.html'
