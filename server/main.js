c0f1b1f9bd5e79c59b82e4adc03a52fc9303a2ed
Vitche Research Team <developer@vitche.com>
2019-08-21 16:47:23 +0000

    1. Server code to initialize test peers on first start.

--- server/main.js
+++ server/main.js
@@ -1,6 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import Peer from '../modules/peer/model';
+import { Peer } from '../modules/peer/model';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    if (0 === Peer.find({}).count()) {
+  	    Peer.insert({
+  	  	    name: "Test network host shell tunnel",
+  	  	    uri: "grpc://peer0.org1.example.com:7053"
+  	    });
+  	    console.log("Created test peers");
+    }
 });
