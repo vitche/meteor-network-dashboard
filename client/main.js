aafe7e24a0ce48e55a5a233cb5c40c09cc40b688
Vitche Research Team <developer@vitche.com>
2019-09-05 16:26:20 +0000

    1. Added routing.

--- client/main.js
+++ client/main.js
@@ -2,5 +2,12 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../modules/peer/client/main'
 import '../modules/peer-edit/client/widget'
 import '../modules/peer-list/client/widget'
+
+Template.main.helpers({
+	peerIdentifier: function() {
+		return FlowRouter.getParam('_id');
+	}
+});
---
+++ modules/peer/client/main.js
@@ -0,0 +1,6 @@
+FlowRouter.route('/peers/:_id', {
+	name: 'peers.edit',
+	action(params, query) {
+		console.log('peers.edit');
+	}
+});
