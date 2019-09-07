615caf87957b14da360fd8ec137c73bd6f987141
vpaki <vpaki@softserveinc.com>
2019-09-07 14:20:37 +0300

    feature/authentication: add basic authentication

--- config/routers/dashboard.routes.js
+++ config/routers/dashboard.routes.js
@@ -2,9 +2,13 @@
  * Routers related to all Dashboard functionality
  */
 
+import { Meteor } from 'meteor/meteor';
+
+FlowRouter.triggers.enter([AccountsTemplates.ensureSignedIn]);
+
 FlowRouter.route('/', {
     name: 'dashboard.list',
-    action:function (params, queryParams) {
-        BlazeLayout.render('layout', {main: 'dashboard'});
-    }
+    action: function (params, queryParams) {
+        BlazeLayout.render('layout', { main: 'dashboard' });
+    },
 });
