9eb0bc792318eae5fbb187de71440794fbb2bc49
Vladyslav Pakin <vpakin@gmail.com>
2019-09-28 13:10:18 +0300

    feature/fork-subgroup: fix group list

--- modules/groups/client/groups-list/groups-list.js
+++ modules/groups/client/groups-list/groups-list.js
@@ -2,15 +2,29 @@ import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict'
 import * as _ from 'lodash';
 
-import { ModalService } from "../../../ui-modal/client/service/modal.service";
+import { GroupsCollection } from '../../both/groups.schema';
+import { GroupService } from '../services/groups.service';
+import { ModalService } from '../../../ui-modal/client/service/modal.service';
 
 import './groups-list.html';
 import '../group-item/group-item';
 
 Template.groupsListWidget.onCreated(function () {
 	this.state = new ReactiveDict();
-
 	this.groupsList = [];
+	this.selectedGroupUsers = [];
+
+	// TODO: catch error?
+
+	const groupsHandler = this.subscribe('groups.publish.getGroupList');
+
+	this.autorun(() => {
+		if (groupsHandler.ready()) {
+			this.groupsList = GroupsCollection.find().fetch();
+			this.state.set('isGroupsLoading', true);
+		}
+	});
+
 
 	this._init = () => {
 		this.state.set('isGroupsLoading', false);
@@ -24,37 +38,21 @@ Template.groupsListWidget.onCreated(function () {
 		});
 	};
 
-	this.onSelectGroup = (groupId) => {
-		const group = _.find(this.groupsList, (group) => group._id === groupId);
-		Meteor.call('groups.method.getGroupUsers', { groupId }, (err, result) => {
-			if (err) {
-				throw new Error(err);
-			}
-
-			const users = result.map((user) => {
-				return {
-					_id: user._id,
-					email: user.emails[0].address,
-					firstName: user.profile.firstName,
-					lastName: user.profile.lastName
-				}
-			});
-
-			this.state.set('selectedGroup', { group, users });
-		});
+	this.onSelectGroup = () => {
+		const group = Template.instance().data.group;
+		GroupService.getGroupUsers(group._id, (users) => {
+			this.selectedGroupUsers = users;
+			this.state.set('selectedGroup', { group, users })
+		})
 	};
 
-	this.addChildGroup = (groupId) => {
-		const parentGroup = _.find(this.groupsList, (group) => group._id === groupId);
-		// TODO: make it as extended service for modals;
-		ModalService.forkSubgroup(parentGroup)
+	this.addChildGroup = () => {
+		ModalService.forkSubgroup({ parentGroup: Template.instance().data.group })
 	};
 
-	this.deleteGroup = (groupId) => {
-		// TODO: show modal window with confirmation;
+	this.deleteGroup = () => {
 	};
 
-	this._init();
 });
 
 Template.groupsListWidget.helpers({
@@ -65,7 +63,7 @@ Template.groupsListWidget.helpers({
 		return Template.instance().state.get('selectedGroup');
 	},
 	groupsList: function () {
-		return Template.instance().groupsList;
+		return GroupsCollection.find();
 	},
 	groupItem: function (group) {
 		const instance = Template.instance();
