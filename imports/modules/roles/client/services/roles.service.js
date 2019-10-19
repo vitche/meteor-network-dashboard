0c21ff7041935491dd4139621e647927b879f26e
Vlad Pakin <vpakin@gmail.com>
2019-10-19 10:01:16 +0300

    feature/setup-profile: fix access to invite user, fix error with full profile name

---
+++ imports/modules/roles/client/services/roles.service.js
@@ -0,0 +1,19 @@
+import { ROLES_DICTIONARY } from '../../../../configs/roles/roles.dictionary';
+import { SERVER_SESSIONS_KEYS } from '../../../../configs/server-session.keys';
+
+class RolesServiceClass {
+	constructor() {
+		this.permissions = [];
+	}
+
+	getPermissions() {
+		return ServerSession.get(SERVER_SESSIONS_KEYS.userPermissions);
+	}
+
+	isAllowToInviteUsers() {
+		return this.getPermissions().indexOf(ROLES_DICTIONARY.private.organizationOwner.alias) > -1;
+	}
+
+}
+
+export const RolesService = new RolesServiceClass();
